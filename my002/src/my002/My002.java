adab408b07871c8a5bd2658e805aa3c7e2c20e55
davidyuibm <65706081+davidyuibm@users.noreply.github.com>
2022-08-19 20:56:06 +0800

    Update My002.java

--- my002/src/my002/My002.java
+++ my002/src/my002/My002.java
@@ -8,6 +8,7 @@ public class My002 {
 		System.out.print("Hello World by David Yu");
 		System.out.print("Test Git on 8/19");
 		System.out.print("Remember to use personal token");
+		System.out.print("Say Hello to every one from Taiwan");
 		
 	}
 
